a59ed46347007b84032ff47af5583a98af261f4d
ozdoganic <ozdoganic@gmail.com>
2026-03-03 22:49:19 +0300

    Deneme 2

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Ibrahim Can Ozdogan';
+export const SITE_DESCRIPTION = 'Automation Software & IIoT Engineer — blog, projects, and case studies.';
